6dae8455829ad5ee714fbe677f4d0864d4103ffe
Edward Withers <dearshrewdwit@gmail.com>
2020-08-25 12:47:00 +0100

    Update simple_usage_spec.rb

--- spec/simple_usage_spec.rb
+++ spec/simple_usage_spec.rb
@@ -11,3 +11,23 @@ describe Animal do
     expect(animal.sleep).to eq 'snore'
   end
 end
+
+
+describe 'using a string description' do
+  # what happens now?
+  subject(:animal) { described_class.new }
+
+  it 'can roar' do
+    expect(animal.roar).to eq 'ROAR!'
+  end
+  it 'can snore' do
+    expect(animal.sleep).to eq 'snore'
+  end
+end
+
+describe 'using subject' do
+  it 'can roar' do
+    # use p() to investigate what the return value of #subject is
+    expect(subject.roar).to eq 'ROAR!'
+  end
+end
